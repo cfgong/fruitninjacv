61051bc6cb3b09c51a6272da3e5b85039f3b9fc8
Crystal Gong <crystalgong123@gmail.com>
2018-12-05 20:52:27 -0600

    added game code to finger detection code, formatted text, cleaned code

--- newFingerDetection.py
+++ newFingerDetection.py
@@ -1,73 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Dec  5 15:52:30 2018
-
-@author: cfg9740
-"""
-
-# -*- coding: utf-8 -*-
-"""
 EECS 332 Fruit Ninja Final Project
-written by Alvin Tan
+written by Alvin Tan & Crystal Gong
 
-This file will eventually include declarations for objects and a live-stream
-with the ability to overlay randomly generated objects over it.
+Fruit Ninja Game
 """
 
 import cv2
 import numpy as np
+from gameObjects import Text, Level, randomFruit, randomBomb
 
-GRAVITY = 4
 NUMFRUITS = 3
-FONT = cv2.FONT_HERSHEY_COMPLEX
-FONT_SIZE = 1.5
 STALL = 5
-CIRCLE_SIZE = 10
+LEVEL_START_PAUSE = 20
+GAME_TITLE = "Squares are Bombs!"
 
-# These determine the positions to generate "exploded" bits of the fruit
-EXX = [1, 0.70711, 0, -0.70711, -1, -0.70711, 0, 0.70711]
-EXY = [0, 0.70711, 1, 0.70711, 0, -0.70711, -1, -0.70711]
-EXV = 8
+# font color
+GREEN = (0,255,0) #(B, G, R) 
+RED = (0,0,255) #(B, G, R) 
 
-class Fruit:
-    def __init__(this, radius, color, xPos, yPos, xVel, yVel):
-        this.radius = radius
-        this.color = color
-        this.xPos = xPos
-        this.yPos = yPos
-        this.xVel = xVel
-        this.yVel = yVel
-        
-    def draw(this, frame):
-        cv2.circle(frame, (this.xPos, this.yPos), this.radius, this.color, -1)
-        
-    def doPhysics(this, xBound, yBound):
-        this.xPos += this.xVel
-        this.yPos += this.yVel
-        this.yVel += GRAVITY
-        
-        return this.xPos >= 0 and this.xPos <= xBound and this.yPos <= yBound
-    
-    def explode(this, explosions):
-        exRad = int(this.radius/2)
-        explosions.append(Fruit(exRad, this.color, this.xPos, this.yPos, this.xVel, this.yVel))
-        for i in range(8):
-            explosions.append(Fruit(exRad, this.color, this.xPos + int(EXX[i]*exRad), this.yPos + int(EXY[i]*exRad), this.xVel + int(EXX[i]*EXV), this.yVel + int(EXY[i]*EXV)))
-    
-    def isAbove(this, yVal):
-        return this.yPos < yVal
-    
-    def intersects(this, fingerTip):
-        return (abs(fingerTip[0] - this.yPos) <= this.radius) and (abs(fingerTip[1] - this.xPos) <= radius)
-    
-class Text:
-    def __init__(this, pos, color, text):
-        this.pos = pos
-        this.color = color
-        this.text = text
-        
-    def write(this, frame):
-        cv2.putText(frame, this.text,this.pos, FONT, FONT_SIZE, this.color, 2, cv2.LINE_AA)
         
 """
 code for getting finger tip using contours
@@ -106,7 +57,7 @@ use ROI selection to get min and max thresholds
 def selectSkin(frame, skinSample, min_YCrCb, max_YCrCb):
     # select the bounding box of the object we want to track (make
     # sure you press ENTER or SPACE after selecting the ROI)
-    r = cv2.selectROI("Fruit Ninja", frame, fromCenter=False, showCrosshair=True)
+    r = cv2.selectROI(GAME_TITLE, frame, fromCenter=False, showCrosshair=True)
     if max(r) > 0: # make sure a box was actually selected or else we will get an error
         skin = frame[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
         # Convert skin sample image to YCrCb
@@ -116,9 +67,13 @@ def selectSkin(frame, skinSample, min_YCrCb, max_YCrCb):
         max_YCrCb = np.max(skinSample, axis = 1)[0] 
     return skinSample, min_YCrCb, max_YCrCb
 
+def exit(vidFeed):
+    print("exited the loop")
+    vidFeed.release()
+    print("released video feed")
+    cv2.destroyAllWindows() # For some reason python always crashes here
 
-
-""" ACTUAL CODE STARTS HERE """
+""" ACTUAL GAME CODE STARTS HERE """
 if __name__ == "__main__":
     # We open a new window and open access to the video camera
     vidFeed = cv2.VideoCapture(0)
@@ -132,78 +87,32 @@ if __name__ == "__main__":
     
     x0 = int(vidFeed.get(3)) # Gets the width of the video feed
     y0 = int(vidFeed.get(4)) # Gets the length of the video feed
-    
-    # We instantiate an empty list of fruit, to be populated in the following
-    # for-statement.
-    fruits = []
-    explodedBits = []
-    
-    for count in range(NUMFRUITS):
-        # Generates reasonable random values for the initialization of each fruit
-        radius = np.random.randint(20, 70)
-        color = (np.random.randint(255), np.random.randint(255), np.random.randint(255))
-        xPos = np.random.randint(x0)
-        yPos = y0
-        xVel = int(((x0/2)-xPos)//10)
-        yVel = -np.random.randint(y0//20, y0//10)
-        fruits.append(Fruit(radius, color, xPos, yPos, xVel, yVel))
 
-
-    # We start off without a histogram
+    # We start off without a skin sample
     test = None
     skinSample = None
     # skin max and min
     min_YCrCb = np.array([0,0,0],np.uint8)
     max_YCrCb = np.array([255,255,255],np.uint8)
-        
-    # This is the setup time, when we have not yet gotten a histogram yet
-    while True:
+    
+    # keep track if game ended
+    gameOver = False
+    
+    # This is the setup time, when we have not yet gotten a skin sample
+    while skinSample is None:
         frame = cv2.flip(frame, 1)
         # After displaying the frame, we grab a new frame from the video feed
         gotFrame, frame = vidFeed.read()
         
-        if skinSample is not None:
-            fingerTip = getFingerTip(frame, min_YCrCb, max_YCrCb)
-                    
-            for count in range(NUMFRUITS):
-                # we draw it on screen
-                fruits[count].draw(frame)
-                
-                # Just to test out explosions, if the fruit reaches halfway, it explodes
-                if fruits[count].intersects(fingerTip):
-                    fruits[count].explode(explodedBits)
-                    
-                    # generates a new fruit to replace it
-                    radius = np.random.randint(20, 70)
-                    color = (np.random.randint(255), np.random.randint(255), np.random.randint(255))
-                    xPos = np.random.randint(x0)
-                    yPos = y0
-                    xVel = int(((x0/2)-xPos)//10)
-                    yVel = -np.random.randint(y0//20, y0//10)
-                    fruits[count] = Fruit(radius, color, xPos, yPos, xVel, yVel)
-                
-                # and increment the physics. If the fruit goes offscreen, we remove it
-                # and implement another randomly generated fruit in its place
-                if not fruits[count].doPhysics(x0, y0):
-                    #print("a fruit died")
-                    radius = np.random.randint(20, 70)
-                    color = (np.random.randint(255), np.random.randint(255), np.random.randint(255))
-                    xPos = np.random.randint(x0)
-                    yPos = y0
-                    xVel = int(((x0/2)-xPos)//10)
-                    yVel = -np.random.randint(y0//20, y0//10)
-                    fruits[count] = Fruit(radius, color, xPos, yPos, xVel, yVel)
+        # write starting text
+        Text((120, 20), GREEN, "Welcome to "+str(GAME_TITLE)).write(frame)
+        Text((115, 45), GREEN, "Hit the circles, avoid the bombs!").write(frame)
+        Text((95, y0-35), GREEN, "Press 'S' to select hand skin sample").write(frame)
+        Text((105, y0-10), GREEN, "Then, press 'ENTER' to begin game").write(frame)
         
-            for exBit in explodedBits:
-                exBit.draw(frame)
-                if not exBit.doPhysics(x0, y0):
-                    explodedBits.remove(exBit)
-        
-            # Draw in the detected fingertip for troubleshooting purposes
-            cv2.circle(frame,(fingerTip[0], fingerTip[1]), 10, (0,255,0), -1)
         
         # Once all the fruits have been drawn on the frame, we display the frame
-        cv2.imshow("Fruit Ninja", frame)
+        cv2.imshow(GAME_TITLE, frame)
         
         key = cv2.waitKey(STALL) & 0xFF
         # select bounding box for skin sample
@@ -211,11 +120,153 @@ if __name__ == "__main__":
             skinSample, min_YCrCb, max_YCrCb = selectSkin(frame, skinSample, min_YCrCb, max_YCrCb)
         # quit
         elif key == ord('q'): # Exit on q
-            break
+            exit(vidFeed)
+    
+    # Here we instantiate our levels:
+    levels = []
+    levels.append(Level(1, 2, 0, 500)) # numFruits, pointsPerFruit, numBombs, numFrame
+    levels.append(Level(3, 5, 0, 500))
+    levels.append(Level(3, 5, 1, 500))
+    levels.append(Level(5, 7, 3, 500))
+    
+    # We instantiate our score and our lives
+    score = 0
+    lives = 3
 
-# Once an interrupt key is entered, or we fail to get another screen, 
-# we print some statements, release the video feed, and close the window.
-print("exited the loop")
-vidFeed.release()
-print("released video feed")
-cv2.destroyAllWindows() # For some reason python always crashes here
+    # For each of the levels, we do game stuff
+    for curLevel in range(len(levels)):
+        if gameOver: # if the game is over, we just break
+            break
+        # We instantiate our game objects
+        texts = []
+        fruits = []
+        bombs = []
+        explodedBits = []
+    
+        # We generate the text for this level
+        texts.append(Text((10, 50), GREEN, "Lives: "))
+        texts.append(Text((int(x0/2), 50), GREEN, "Level: " + str(curLevel)))
+        texts.append(Text((x0 - 150, 50), GREEN, "Score: "+str(score)))
+        
+        # We generate all of the fruits in this level
+        for count in range(levels[curLevel].numFruits):
+            fruits.append(randomFruit(x0, y0))
+        
+        # We generate all of the bombs in this level
+        for count in range(levels[curLevel].numBombs):
+            bombs.append(randomBomb(x0, y0))
+        
+        startPause = 0
+        while (startPause < LEVEL_START_PAUSE) and gotFrame:
+            # Flips the frame horizontally so it's a mirror image. Makes sense while
+            # facing the screen.
+            frame = cv2.flip(frame, 1)
+            
+            Text((int(x0/2)-100, int(y0/2)), GREEN, GAME_TITLE).write(frame)
+            Text((int(x0/2)-45, int(y0/2)+25), GREEN, "LEVEL "+str(curLevel)).write(frame)
+            
+            cv2.imshow(GAME_TITLE, frame)
+            startPause += 1
+            
+            # After displaying the frame, we grab a new frame from the video feed
+            gotFrame, frame = vidFeed.read()
+            # Then, pause for 10 ms to see if we entered an interrupt key or not
+            if cv2.waitKey(STALL) == ord('q'): # Exit on q
+                exit(vidFeed)
+        
+        levelFrames = 0
+        
+        while gotFrame and not gameOver and levelFrames < levels[curLevel].numFrames:
+            # Flips the frame horizontally so it's a mirror image. Makes sense while
+            # facing the screen.
+            frame = cv2.flip(frame, 1)
+            
+            # Grab the coordinate of the player's fingertip
+            fingerTip = getFingerTip(frame, min_YCrCb, max_YCrCb)
+            
+            # For each fruit we have,
+            for count in range(len(fruits)):
+                # we draw it on screen
+                fruits[count].draw(frame)
+                
+                # If our fintertip intersects with the fruit, we explode the fruit
+                if fruits[count].intersects(fingerTip):
+                    fruits[count].explode(explodedBits)
+                    
+                    # Update the score and display the new score
+                    score += levels[curLevel].pointsPerFruit
+                    texts[2] = Text((x0 - 150, 50), GREEN, "Score: "+str(score))
+                    
+                    # Generate a new fruit to replace it
+                    fruits[count] = randomFruit(x0, y0)
+                
+                # and increment the physics. If the fruit goes offscreen, we remove it
+                # and implement another randomly generated fruit in its place
+                if not fruits[count].doPhysics(x0, y0):
+                    fruits[count] = randomFruit(x0, y0)
+            
+            # For each bomb we have,
+            for count in range(len(bombs)):
+                # we draw it on screen
+                bombs[count].draw(frame)
+                
+                # If our fingertip intersects with the bomb, we explode the bomb
+                if bombs[count].intersects(fingerTip):
+                    bombs[count].explode(explodedBits)
+                    
+                    # Update the number of lives and checks if you are dead
+                    lives -= 1
+                    if lives <= 0:
+                        gameOver = True
+                    
+                    # Generates a new bomb to replace it
+                    bombs[count] = randomBomb(x0, y0)
+                
+                # and increment the physics
+                if not bombs[count].doPhysics(x0, y0):
+                    bombs[count] = randomBomb(x0, y0)
+            
+            # Write all of our text on the screen
+            for text in texts:
+                text.write(frame)
+                
+            # Display the lives we have
+            for count in range(lives):
+                cv2.circle(frame, (175 + count*50, 35), 20, RED, -1)
+            
+            # Let exploded bits continue falling down
+            for exBit in explodedBits:
+                exBit.draw(frame)
+                if not exBit.doPhysics(x0, y0):
+                    explodedBits.remove(exBit)
+        
+            # Draw in the detected fingertip for troubleshooting purposes
+            cv2.circle(frame,(fingerTip[0], fingerTip[1]), 10, GREEN, -1)
+        
+            # Once all the fruits have been drawn on the frame, we display the frame
+            cv2.imshow(GAME_TITLE, frame)
+            
+            # Increment the frame counter
+            levelFrames += 1
+            
+            # After displaying the frame, we grab a new frame from the video feed
+            gotFrame, frame = vidFeed.read()
+            
+            # Then, pause to see if we entered an interrupt key or not
+            if cv2.waitKey(STALL) == ord('q'): # Exit on q
+                exit(vidFeed)
+    # just need to write text once
+    if gameOver and skinSample is not None:
+        frame = cv2.flip(frame, 1) # just need to flip the frame once
+        Text((int(x0/2) - 70, int(y0/2)), RED, "GAME OVER").write(frame)
+        Text((int(x0/2) - 90, int(y0/2)+25), RED, "FINAL SCORE: "+str(score)).write(frame)
+        
+    # If the player loses and the game was actually started
+    while gameOver and skinSample is not None: 
+        cv2.imshow(GAME_TITLE, frame)
+        
+        if cv2.waitKey(STALL) == ord('q'): # Exit on q
+            exit(vidFeed)
+            break
+            
+    exit(vidFeed)
